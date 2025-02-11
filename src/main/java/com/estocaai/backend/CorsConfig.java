62a20e4a64da9527c2452f18f28689d29a694420
Henrique <henriquessm@al.insper.edu.br>
2025-02-11 00:14:45 -0300

    Descrição das alterações

--- src/main/java/com/estocaai/backend/CorsConfig.java
+++ src/main/java/com/estocaai/backend/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://floating-lowlands-90887-cc961db17145.herokuapp.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
