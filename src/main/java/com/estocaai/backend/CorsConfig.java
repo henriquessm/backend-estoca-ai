1ec55e4c291c71ec651ba90a90ba5a5aac8527d4
Henrique <henriquessm@al.insper.edu.br>
2025-02-11 00:19:38 -0300

    Descrição das alterações

--- src/main/java/com/estocaai/backend/CorsConfig.java
+++ src/main/java/com/estocaai/backend/CorsConfig.java
@@ -1,10 +1,3 @@
-package com.estocaai.backend;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig {
 
@@ -14,7 +7,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://floating-lowlands-90887-cc961db17145.herokuapp.com")
+                        .allowedOrigins("https://floating-lowlands-90887.herokuapp.com") // Domínio do Heroku
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
